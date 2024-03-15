6c89e22d2cbfe4a1b05ec5ab5a86c6d19f6926d2
Ali <nabavi1397@gmail.com>
2024-03-14 20:41:43 -0700

    git initialization

---
+++ dentalinformationsystem/src/main/java/org/alinabavi/dentalinformationsystem/DentalinformationsystemApplication.java
@@ -0,0 +1,13 @@
+package org.alinabavi.dentalinformationsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DentalinformationsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DentalinformationsystemApplication.class, args);
+	}
+
+}
---
+++ dentalinformationsystem/src/test/java/org/alinabavi/dentalinformationsystem/DentalinformationsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package org.alinabavi.dentalinformationsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DentalinformationsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
